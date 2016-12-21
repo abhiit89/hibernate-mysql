11b48e2a6bb73a445066c98baf748873c3d1fb87
abhiit89 <abhi.aec89@gmail.com>
2016-12-21 09:37:35 +0530

    REST API finished

--- src/main/java/com/mkyong/common/Resource.java
+++ src/main/java/com/mkyong/common/Resource.java
@@ -1,18 +1,17 @@
 package com.mkyong.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.mkyong.persistence.HibernateUtil;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.Session;
-
+import com.google.gson.Gson;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -26,30 +25,22 @@ public class Resource {
     @Path("read")
     @Produces(MediaType.APPLICATION_JSON)
     public Response readData() throws JSONException, JsonProcessingException {
-        StringBuilder apiReadString = new StringBuilder();
         JSONObject apiReadObject = new JSONObject();
         Session session1 = HibernateUtil.getSessionFactory().openSession();
         session1.beginTransaction();
         List list = session1.createQuery(" from Stock").list();
         Iterator itr = list.iterator();
+        ArrayList<Stock> data = new ArrayList<Stock>();
         while(itr.hasNext())
         {
             Stock user = (Stock)itr.next();
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            String json = ow.writeValueAsString(user);
-            apiReadString.append(json);
-            apiReadString.append(",");
-        }
-        final int length = apiReadString.length();
-        if ( length > 0 ) {
-            // We remove the last character.
-            apiReadString.deleteCharAt( length - 1 );
+            data.add(user);
         }
         session1.getTransaction().commit();
         session1.close();
-        apiReadObject.append("data", apiReadString.toString());
-        return Response.ok() //200
-                .entity(apiReadObject.toString())
+        Gson gson = new Gson();
+        return Response.ok()
+                .entity(gson.toJson(data))
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "*")
                 .allow("OPTIONS").build();
